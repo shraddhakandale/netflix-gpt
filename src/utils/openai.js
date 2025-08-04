161690d74037432635ab3b012955d29b0354f497
shraddhakandale <sorkeyy@gmail.com>
2025-08-04 20:29:31 +0530

    added open ai key

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { OPEN_AI_KEY } from './constants';
 
 const openai = new OpenAI({
-  apiKey: "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", // defaults to process.env["OPENAI_API_KEY"]
+  apiKey: process.env.REACT_APP_OPEN_AI_KEY,
   dangerouslyAllowBrowser: true,
 });
 
